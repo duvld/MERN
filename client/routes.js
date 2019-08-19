de00a8f35d27ea7d89d3c99e8d108f95f4b3780a
duvld <ollejna@gmail.com>
2019-08-19 12:49:33 -0400

    insert wokring

---
+++ client/routes.js
@@ -0,0 +1,15 @@
+// you are cute and i like ur brain
+
+//client/routes.js
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import App from './components/app';
+
+export const Routes = () => (
+    <Switch>
+      <Route exact path='/' component={App} />
+    </Switch>
+);
+
+
+export default Routes;
